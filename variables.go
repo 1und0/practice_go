9672d62922526a08f18c6ea1a377263d4ec8c98c
1und0 <1und0@mail.com>
2021-07-20 13:49:55 +0200

    test basic variable declarations

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 1
+	var b, c int = 2, 3
+	var d = true
+	var e string = "is this"
+	short := "thats neat"
+
+	fmt.Println(a + b + c)
+	fmt.Println(e, d)
+	fmt.Println(short)
+}
